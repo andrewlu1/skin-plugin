9bb5e41f3efe5adeeb670cd5f1641c0b56bd151b
andrewlu.lzw <andrewlu.lzw@alibaba-inc.com>
2018-01-21 00:11:33 +0800

    上传皮肤编译插件代码。用于编译期生成特定的皮肤资源。

---
+++ skinplugin/src/main/groovy/cn.andrewlu.skinplugin/SkinPluginImpl.groovy
@@ -0,0 +1,22 @@
+package cn.andrewlu.skinplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SkinPluginImpl implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        project.afterEvaluate {
+            TaskHandler handler = new TaskHandler()
+            project.tasks.each { task ->
+                task.doLast {
+                    project.android.applicationVariants.all { variant ->
+                        handler.handle(project, task, variant.name)
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ skinplugin/src/main/groovy/cn.andrewlu.skinplugin/TaskHandler.groovy
@@ -0,0 +1,58 @@
+package cn.andrewlu.skinplugin
+
+import org.gradle.api.Project
+import org.gradle.api.Task;
+
+class TaskHandler {
+    public TaskHandler() {
+    }
+
+    public void handle(Project project, Task task, String variant) {
+        //generateReleaseSources
+        //processDebugResources
+        String taskName = "process${variant.charAt(0).toUpperCase()}${variant.substring(1)}Resources"
+        if (task.name == taskName) {
+            println("${task}:start process Resource with R.txt >>>>>>>>>>>>>>")
+            if (task.outputs.hasOutput) {
+                for (File file : task.outputs.files) {
+                    println("task.outputs.file:${file.absolutePath}")
+                    if (!file.exists()) return
+
+                    if (file.name == "R.txt") {
+                        File assetDir = new File(project.android.sourceSets.main.assets.srcDirs.path.getAt(0))
+                        File skinDir = new File(assetDir, "skin")
+                        boolean mkdir = skinDir.mkdirs()
+                        File dataFile = new File(skinDir, "data")
+                        println("dataFile.createNewFile:${dataFile.absolutePath},$mkdir")
+                        FileInputStream inputStream = new FileInputStream(file)
+                        DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(dataFile))
+                        List<String> lines = inputStream.readLines()
+
+                        lines.each { line ->
+                            String[] split = line.split(" ")
+                            if (split.length < 4) return
+                            if (split[0].endsWith("[]")) return
+                            try {
+                                outputStream.writeInt(split[2].hashCode())
+                                if (split[3].startsWith("0x")) {
+                                    outputStream.writeInt(Integer.valueOf(split[3].substring(2), 16))
+                                } else {
+                                    outputStream.writeInt(Integer.valueOf(split[3]))
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace()
+                                println("resource:${split[2]},failed to generate resId!")
+                            }
+                        }
+                        inputStream.close()
+                        outputStream.close()
+                        break
+                    }
+                }
+            } else {
+                println("task:${task} has no outputFiles")
+            }
+            println(">>>>>>>>>>>>>>  end process Resource with R.txt")
+        }
+    }
+}
